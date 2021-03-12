b4924650836629b36b67d3908f9b6e1f9a6d005b
taylor <taylor@teukka.com>
2021-03-12 15:05:08 +0200

    initial commit

---
+++ api/[site].js
@@ -0,0 +1,33 @@
+import multer from "multer";
+import axios from "axios";
+import qs from "querystring";
+
+const upload = multer();
+const config = require("../config.json");
+
+async function pubHandler(req, res) {
+  const token =
+    req.headers["Authorization"].split(" ")[1] || req.body.access_token;
+  if (!token) {
+    res.sendStatus(401);
+  }
+  const { text } = await axios.get(config.tokenValidationEndpoint, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "User-Agent": `${config.appName}`,
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const { me, scope } = qs.parse(text);
+  if (!me || !scope) {
+    res.status(401).send("Invalid Token");
+  }
+  // const { h } = request.body;
+  console.log(request.body);
+  // only support h-entry types for now
+  // if (h === "entry") {
+
+  // }
+}
+
+module.exports = upload.array()(pubHandler);
