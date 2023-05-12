21ea63ec43de753ef5369f143f053dd0b2d30af8
joshers <josh@darkfox.io>
2023-05-11 21:25:36 -0400

    Update getPublishPath.js

--- getPublishPath.js
+++ getPublishPath.js
@@ -2,13 +2,7 @@ const { generateSlug } = require("./compileContent.js");
 
 function getPublishPath(fields) {
   const slug = generateSlug(fields);
-  if (fields["bookmark-of"]) {
-    return `content/bookmarks/${slug}.md`;
-  }
-  if (fields.body) {
-    return `content/posts/${slug}.md`;
-  }
-  return `content/updates/${slug}.md`;
+  return `content/posts/${slug}.md`;
 }
 
 module.exports = getPublishPath;
